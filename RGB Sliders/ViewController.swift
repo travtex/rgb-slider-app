8783a1d458cd4c4bf5f591a345e72fea0f032199
Travis Flatt <tflatt@parlevelsystems.com>
2015-06-08 11:21:37 -0500

    Add navigation controller

--- RGB Sliders/ViewController.swift
+++ RGB Sliders/ViewController.swift
@@ -13,10 +13,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var colorSquare: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        colorSquare.layer.borderColor = UIColor.blackColor().CGColor
+        colorSquare.layer.borderWidth = 1
         updateBackgroundColor()
     }
 
@@ -31,7 +34,7 @@ class ViewController: UIViewController {
         let green = (CGFloat)(greenSlider.value)
         let blue = (CGFloat)(blueSlider.value)
         
-        view.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        colorSquare.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
        
     }
 
