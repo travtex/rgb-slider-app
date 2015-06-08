192d803f87d17a660f89ea5cc27c1f9194ca1256
Travis Flatt <tflatt@parlevelsystems.com>
2015-06-08 10:43:32 -0500

    Initial Commit

---
+++ RGB Sliders/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RGB Sliders
+//
+//  Created by Travis Flatt on 6/8/15.
+//  Copyright (c) 2015 Travis Flatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ RGB SlidersTests/RGB_SlidersTests.swift
@@ -0,0 +1,36 @@
+//
+//  RGB_SlidersTests.swift
+//  RGB SlidersTests
+//
+//  Created by Travis Flatt on 6/8/15.
+//  Copyright (c) 2015 Travis Flatt. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class RGB_SlidersTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
